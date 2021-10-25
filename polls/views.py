6a510b8962bfc79a7ba21a94da4774d762cc4f49
Icezu <krasin.d@ku.th>
2021-10-25 15:39:13 +0700

    Add login required to vote and welcome message

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import Choice, Question
 
@@ -40,6 +41,7 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
+@login_required
 def vote(request, question_id):
     """Update choice count when selecting."""
     question = get_object_or_404(Question, pk=question_id)
