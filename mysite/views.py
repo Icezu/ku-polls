e6752ebaba5374f26116a7c9611bae2606c3830a
Icezu <krasin.d@ku.th>
2021-10-29 21:51:20 +0700

    Remove unused import

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,5 @@
-from django.contrib import auth
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
