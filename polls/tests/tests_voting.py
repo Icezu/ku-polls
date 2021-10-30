0940deffe9a619e7231d4410edfee926927eff64
Icezu <krasin.d@ku.th>
2021-10-30 21:24:08 +0700

    Add 2 voting test

--- polls/tests/tests_voting.py
+++ polls/tests/tests_voting.py
@@ -20,7 +20,25 @@ def create_question(question_text, start, end):
                                    pub_date=time, end_date=end_time)
 
 
-# class VotingTest(TestCase):
-#     """Test for voting."""
+class VotingTest(TestCase):
+    """Test for voting."""
 
+    def setUp(self):
+        """Setup the test."""
+        User.objects.create_user(username="User", password="Test@1234", email="User@hotmail.com")
+        self.question = create_question(question_text="TestQuestion", start=-1, end=7)
+        self.choice1 = self.question.choice_set.create(choice_text='Choice1')
+        self.choice2 = self.question.choice_set.create(choice_text='Choice2')
+        self.question_url = reverse('polls:vote', args=(self.question.id,))
+
+    def test_response_code_unauthenticated_detail_page(self):
+        """Unauthenticated user cannot access the detail page."""
+        response = self.client.get(self.question_url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_response_code_authenticated_detail_page(self):
+        """Authenticated user can access detail page."""
+        self.client.post(reverse('login'), {'username': 'User', 'password': 'Test@1234'}, follow=True)
+        response = self.client.get(self.question_url)
+        self.assertEqual(response.status_code, 200)
     
